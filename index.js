8dddd1f92ba163aeb6dd320dedaefc045a75a849
Carlotta Frommer <carlotta.eins@gmail.com>
2020-04-10 23:07:59 +0200

    can draw on the canvas but its one consistent line

--- index.js
+++ index.js
@@ -29,19 +29,27 @@ app.get("/", (req, res) => {
     res.redirect("/petition");
 });
 
+// GET /petition
+
 app.get("/petition", (req, res) => {
+    // no cookie is set
     if (!req.cookies.cookie) {
         res.render("petition");
+        // cookie is set
     } else {
         res.redirect("/thanks");
     }
 });
 
+// POST /petition
+
 app.post("/petition", (req, res) => {
+    // parsed input values
     let first = req.body.first;
     let last = req.body.last;
     let signature = req.body.signature;
     if (first != "" && last != "" && signature != "") {
+        // insert the data as values in my signatures table
         db.getFullName(first, last)
             .then(() => {
                 console.log("That worked!");
@@ -49,24 +57,29 @@ app.post("/petition", (req, res) => {
             .catch((err) => {
                 console.log("Error in getFullName: ", err);
             });
+        // set cookie & redirect
         res.cookie("cookie", "signed");
         res.redirect("/thanks");
     } else if (
+        // there is either an error or
         req.statusCode != 200 ||
+        // the values are empty
         (first == "" && last == "" && signature == "placeholder")
     ) {
+        // render the petition template with error helper
         res.render("petition", { error: true });
     }
 });
+// GET /thanks
 
-app.post("/add-city", (req, res) => {
-    db.addCity("Guayaquil", "Ecuador")
-        .then(() => {
-            console.log("Yay that worked!");
-        })
-        .catch((err) => {
-            console.log("Error in addCity: ", err);
-        });
+app.get("/thanks", (req, res) => {
+    // if a cookie is set, render
+    if (req.cookies.cookie) {
+        res.render("thanks");
+        // if not, redirect
+    } else {
+        res.redirect("/petition");
+    }
 });
 
 app.listen(8080, () => console.log("Express server is at your service."));
--- public/script.js
+++ public/script.js
@@ -3,49 +3,59 @@
 
     const canvasContext = $("canvas")[0].getContext("2d");
     let mouseDown = false;
+    let offset = $("canvas").offset();
+    let startLeft;
+    let startTop;
+    let moveLeft;
+    let moveTop;
+    let coordinates = [];
+    // maybe work with two sets of coordinates to prevent
+    // one consistent line-drawing
 
-    $("canvas").on("mousedown", () => {
-        console.log("Someone put a mouse down on the canvas!");
+    $("canvas").on("mousedown", (event) => {
         mouseDown = true;
-        $("canvas").on("mousemove", (ev) => {
-            console.log("Someone is moving a mouse on here!");
-            $("canvas").ready(drawMouseMove(ev));
-            $("canvas").on("mouseup", (event) => {
-                $("canvas").ready(captureMouseUp(event));
-            });
+        startLeft = event.clientX - offset.left;
+        startTop = event.clientY - offset.top;
+        console.log("Left and top mousedown: " + startLeft + ", " + startTop);
+        coordinates.push({ startX: startLeft, startY: startTop });
+        $("canvas").on("mousemove", (e) => {
+            if (mouseDown) {
+                e.stopPropagation();
+                moveLeft = e.clientX - offset.left;
+                moveTop = e.clientY - offset.top;
+                coordinates.push({ x: moveLeft, y: moveTop });
+                console.log(
+                    "Left and top mousemove: " + moveLeft + ", " + moveTop
+                );
+            }
         });
     });
 
-    $("body").on("mousedown", "mousemove", "mouseup", (e) => {
-        e.stopPropagation();
+    $("canvas").on("mouseup", (event) => {
+        mouseDown = false;
+        event.stopPropagation();
+        $("canvas").off("mousemove");
+        console.log("A mouse went up, the magic ends!");
+        console.log("Coordinates: ", coordinates);
     });
 
-    function startDrawMouseDown(event) {
-        startX = newX;
-        startY = newY;
-        newX = event.clientX - event.offsetLeft;
-        newY = event.clientY - event.offsetTop;
-        canvasContext.beginPath();
-        canvasContext.moveTo(newX, newY);
-    }
+    requestAnimationFrame(drawSignature);
 
-    function drawMouseMove(event) {
-        let left = event.pageX;
-        let top = event.pageY;
-        if (mouseDown) {
-            canvasContext.lineTo(left, top);
-            console.log("Left: ", left);
-            console.log("Top: ", top);
-        } else {
-            canvasContext.stroke();
-            canvasContext.endPath();
+    function drawSignature() {
+        // if there is no signing going on, politely request
+        // an AnimationFrame and do nothing
+        if (coordinates.length == 0 || !mouseDown) {
+            requestAnimationFrame(drawSignature);
+            return;
         }
-    }
-
-    function captureMouseUp(event) {
-        event.stopPropagation();
-        console.log("A mouse went up, the magic ends!");
-        $("canvas").off("mousemove");
-        mouseDown = false;
+        // if there is signing going on, animate the signing
+        canvasContext.beginPath();
+        canvasContext.moveTo(coordinates[0].startX, coordinates[0].startY);
+        for (let i = 0; i < coordinates.length; i++) {
+            canvasContext.lineTo(coordinates[i].x, coordinates[i].y);
+        }
+        canvasContext.stroke();
+        canvasContext.closePath(); // somehow this doesn't work
+        requestAnimationFrame(drawSignature);
     }
 })();
